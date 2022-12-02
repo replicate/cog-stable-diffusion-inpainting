ad9d9052851d0807e916ba97310fcd973e9a0d0c
Chenxi <chenxi.whitehouse@gmail.com>
2022-12-02 18:41:32 +0000

    v2

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# cog-stable-diffusion-inpainting
+# cog-stable-diffusion-inpainting-v2
+
+
+[![Replicate](https://replicate.com/replicate/stable-diffusion-inpainting/badge)](https://replicate.com/replicate/stable-diffusion-inpainting) 
+
+This is an implementation of the [Diffusers Stable Diffusion v2](https://huggingface.co/stabilityai/stable-diffusion-2) as a Cog model. [Cog packages machine learning models as standard containers.](https://github.com/replicate/cog)
+
+
+    cog run script/download-weights 
+
+Then, you can run predictions:
+
+    cog predict -i prompt="..." -i image=@... -i mask=@...
---
+++ predict.py
@@ -0,0 +1,86 @@
+import os
+from typing import List
+
+import torch
+from diffusers import DiffusionPipeline
+
+from PIL import Image
+from cog import BasePredictor, Input, Path
+
+MODEL_ID = "stabilityai/stable-diffusion-2-inpainting"
+MODEL_CACHE = "diffusers-cache"
+
+
+class Predictor(BasePredictor):
+    def setup(self):
+        """Load the model into memory to make running multiple predictions efficient"""
+        print("Loading pipeline...")
+        self.pipe = DiffusionPipeline.from_pretrained(
+            MODEL_ID,
+            cache_dir=MODEL_CACHE,
+            local_files_only=True,
+        ).to("cuda")
+
+    @torch.inference_mode()
+    @torch.cuda.amp.autocast()
+    def predict(
+        self,
+        prompt: str = Input(
+            description="Input prompt",
+            default="a photo of an astronaut riding a horse on mars",
+        ),
+        image: Path = Input(
+            description="Inital image to generate variations of. Supproting images size with 512x512",
+        ),
+        mask: Path = Input(
+            description="Black and white image to use as mask for inpainting over the image provided. Black pixels are inpainted and white pixels are preserved",
+        ),
+        prompt_strength: float = Input(
+            description="Prompt strength when providing the image. 1.0 corresponds to full destruction of information in init image",
+            default=0.8,
+        ),
+        num_outputs: int = Input(
+            description="Number of images to output. Higher number of outputs may OOM.",
+            ge=1,
+            le=8,
+            default=1,
+        ),
+        num_inference_steps: int = Input(
+            description="Number of denoising steps", ge=1, le=500, default=25
+        ),
+        guidance_scale: float = Input(
+            description="Scale for classifier-free guidance", ge=1, le=20, default=7.5
+        ),
+        seed: int = Input(
+            description="Random seed. Leave blank to randomize the seed", default=None
+        ),
+    ) -> List[Path]:
+        """Run a single prediction on the model"""
+        if seed is None:
+            seed = int.from_bytes(os.urandom(2), "big")
+        print(f"Using seed: {seed}")
+
+        image = Image.open(image).convert("RGB").resize((512, 512))
+        extra_kwargs = {
+            "mask_image": Image.open(mask).convert("RGB").resize(image.size),
+            "image": image,
+            "strength": prompt_strength,
+        }
+
+        generator = torch.Generator("cuda").manual_seed(seed)
+
+        output = self.pipe(
+            prompt=[prompt] * num_outputs if prompt is not None else None,
+            guidance_scale=guidance_scale,
+            generator=generator,
+            num_inference_steps=num_inference_steps,
+            **extra_kwargs,
+        )
+
+        output_paths = []
+        for i, sample in enumerate(output.images):
+            output_path = f"/tmp/out-{i}.png"
+            sample.save(output_path)
+            output_paths.append(Path(output_path))
+
+        return output_paths
